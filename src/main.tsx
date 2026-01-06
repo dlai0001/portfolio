fe75e69f6bbd9b7636a6992abdd9953863830f4e
David Lai <david@dlaisoft.com>
2026-01-05 20:29:21 -0500

    setup mui

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light', // or 'dark'
+    primary: {
+      main: '#1976d2',
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
