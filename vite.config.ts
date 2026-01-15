816d771a942679b46580052f50b7194c38851393
David Lai <david@dlaisoft.com>
2026-01-14 23:08:47 -0500

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { ViteDevServer } from 'vite'
+import type { Connect } from 'vite'
+import type { ServerResponse } from 'node:http';
 
 // Custom plugin for client-side routing fallback
 const htmlFallback = {
   name: 'html-fallback',
-  configureServer(server) {
-    server.middlewares.use((req, res, next) => {
+  configureServer(server: ViteDevServer) {
+    server.middlewares.use((req: Connect.IncomingMessage, _res: ServerResponse, next: Connect.NextFunction) => {
       // If the request URL is within the base path and not for a known file
-      if (req.url == '/portfolio') {
+      if (req.url === '/portfolio') {
         // Rewrite the URL to the base index.html
         req.url = '/portfolio/'; 
       }
